b053dba4788051f84a26beabc6acf89d236e51b1
Neyda Jimenez <23308060610175@cetis61.edu.mx>
2025-10-27 20:30:44 -0600

    Actualizacion

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "clave_secreta_segura_123" 
+
 
 @app.route('/')
 def inicio():
@@ -17,6 +19,11 @@ def vehiculos():
 @app.route('/maravillas')
 def maravillas():
     return render_template('maravillas.html', title="Las Maravillas del Mundo")
+
+@app.route('/acerca')
+def acerca():
+    return render_template('acerca.html', title="Acerca de...")
+
 
 @app.route("/registrando", methods=("GET", "POST"))
 def registro():
@@ -40,9 +47,27 @@ def registro():
 def registro_exitoso(nombre):
     return render_template('registro_exitoso.html', nombre=nombre)
 
-@app.route('/acerca')
-def acerca():
-    return render_template('acerca.html', title="Acerca de...")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        contrasena = request.form['contrasena']
+
+        if usuario and contrasena:
+            session['usuario'] = usuario
+            return redirect(url_for('inicio'))
+        else:
+            error = "Usuario o contraseña incorrectos."
+
+    return render_template('login.html', title="Iniciar Sesión", error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('inicio'))
 
 if __name__ == '__main__':
     app.run(debug=True)
